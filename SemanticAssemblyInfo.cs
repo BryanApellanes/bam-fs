6da2693533aa139cea70847da5d708ebc7f0ad1a
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-01-24 15:31:44 -0800

    initial commit

---
+++ SemanticAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using Bam.Net;
+
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyCommit("b662bc2")]
+[assembly: AssemblySemanticVersion("1.0.5-test+b662bc2")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+b662bc2, Revision=1.0.5.4109286558")]
+[assembly: AssemblyProduct("bamfs")]
