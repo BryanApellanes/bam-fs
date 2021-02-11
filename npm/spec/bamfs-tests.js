8013307213ca680ff626480e4857d02542d2271f
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-02-11 11:02:00 -0800

    add npm related files

--- npm/spec/bamfs-tests.js
+++ npm/spec/bamfs-tests.js
@@ -1,5 +1,15 @@
 var { expect } = require("chai"),
-    bamfs = require('../bamfs');
+    bamfs = require('../bamfs'),
+    fs = require('fs'),
+    path = require('path'),
+    shell = require('shelljs');
+
+function createTestFiles(){
+    if(fs.existsSync('./test_data')){
+        shell.rm('-fr', './test_data');
+    }
+    
+}
 
 describe("bam fs", function () {
     it("should pass", function(){});
