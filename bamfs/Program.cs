cba9cd00297f077e175ec17a43c2945f96bb130a
Bryan Apellanes <bryan@threeheadz.com>
2024-04-03 22:11:59 -0500

    wip

---
+++ bamfs/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Bam.Console;
+
+namespace Bam.Net.Application
+{
+    [Serializable]
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            BamConsoleContext.Main(args);
+        }
+    }
+}
